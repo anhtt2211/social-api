58b71b6df1485fb2af165e9485a82b418392daec
anhtt2211 <trananh22112001@gmail.com>
2022-12-08 23:49:44 +0700

    first commit

---
+++ src/user/user.interface.ts
@@ -0,0 +1,18 @@
+export interface UserData {
+  username: string;
+  email: string;
+  token: string;
+  bio: string;
+  image?: string;
+}
+
+export interface UserRO {
+  user: UserData;
+}
+
+export interface CurrentUser {
+  id: number;
+  username: string;
+  email: string;
+  exp: number;
+}
