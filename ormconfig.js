d8a8becc71cb498570ff4e03266a72332e0321b2
anhtt2211 <trananh22112001@gmail.com>
2022-12-09 15:53:21 +0700

    update config

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  type: process.env.DATABASE_ENGINE,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_POST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  logging: process.env.TYPEORM_LOGGING === 'true' ? true : false,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true' ? true : false,
+  migrationRun: process.env.TYPEORM_MIGRATION_RUN,
+  migrationsTableName: "migrations",
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+  },
+};
---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const SECRET = "jwt-secret-key";
