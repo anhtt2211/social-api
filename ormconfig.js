572e2a5f1961a8fd0c85d68127ee592a49194279
anhtt2211 <trananh22112001@gmail.com>
2022-12-18 20:00:26 +0700

    wip

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
   type: process.env.DATABASE_ENGINE,
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_POST,
+  port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   entities: [process.env.TYPEORM_ENTITIES],
   migrations: [process.env.TYPEORM_MIGRATIONS],
-  logging: process.env.TYPEORM_LOGGING === 'true' ? true : false,
-  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true' ? true : false,
+  logging: process.env.TYPEORM_LOGGING === "true" ? true : false,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === "true" ? true : false,
   migrationRun: process.env.TYPEORM_MIGRATION_RUN,
   migrationsTableName: "migrations",
   cli: {
--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   Controller,
   Post,
   UploadedFile,
