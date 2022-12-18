11106d69c67978791c57bc6bb5a52ff924beec74
anhtt2211 <trananh22112001@gmail.com>
2022-12-18 18:34:18 +0700

    wip

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -4,14 +4,13 @@ import {
   NestModule,
   RequestMethod,
 } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthMiddleware } from "../user/auth.middleware";
 import { UserModule } from "../user/user.module";
 import { DropboxService } from "./dropbox.service";
 import { MediaController } from "./media.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([]), UserModule],
+  imports: [UserModule],
   providers: [DropboxService],
   controllers: [MediaController],
   exports: [DropboxService],
@@ -20,6 +19,6 @@ export class MediaModule implements NestModule {
   public configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(AuthMiddleware)
-      .forRoutes({ path: "/img/upload", method: RequestMethod.POST });
+      .forRoutes({ path: "media/img/upload", method: RequestMethod.POST });
   }
 }
