ad9ea29b2295bfca0b3b0f99a66f6dad219553f6
anhtt2211 <trananh22112001@gmail.com>
2022-12-18 21:18:06 +0700

    parseInt

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: process.env.DATABASE_ENGINE,
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
