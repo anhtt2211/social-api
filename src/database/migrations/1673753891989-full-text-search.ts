9f1e578f5dbca2ac46ee25fa3afa1ec5be8b385f
anhtt2211 <trananh22112001@gmail.com>
2023-01-15 11:17:31 +0700

    wip

--- src/database/migrations/1673753891989-full-text-search.ts
+++ src/database/migrations/1673753891989-full-text-search.ts
@@ -8,22 +8,27 @@ export class fullTextSearch1673753891989 implements MigrationInterface {
       `ALTER TABLE "article" ADD "document_with_weights" tsvector`
     );
     await queryRunner.query(`
-    update article set document_with_weights = setweight(to_tsvector(title), 'A') ||
-    setweight(to_tsvector(coalesce(description, '')), 'B');
-CREATE INDEX document_weights_idx
-  ON article
-  USING GIN (document_with_weights);
-        CREATE FUNCTION article_tsvector_trigger() RETURNS trigger AS $$
-begin
-  new.document_with_weights :=
-  setweight(to_tsvector(coalesce(new.title, '')), 'A')
-  || setweight(to_tsvector(coalesce(new.description, '')), 'B');
-  return new;
-end
-$$ LANGUAGE plpgsql;
-CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-    ON article FOR EACH ROW EXECUTE PROCEDURE article_tsvector_trigger();
-        `);
+      UPDATE article
+      SET document_with_weights = setweight(to_tsvector(title), 'A') || setweight(to_tsvector(coalesce(description, '')), 'B');
+
+      CREATE INDEX document_weights_idx ON article USING GIN (document_with_weights);
+
+      CREATE FUNCTION article_tsvector_trigger() RETURNS TRIGGER AS $$
+      begin
+        new.document_with_weights :=
+        setweight(to_tsvector(coalesce(new.title, '')), 'A')
+        || setweight(to_tsvector(coalesce(new.description, '')), 'B');
+        return new;
+      end
+      $$ LANGUAGE PLPGSQL;
+
+      CREATE TRIGGER tsvectorupdate
+      BEFORE
+      INSERT
+      OR
+      UPDATE ON article
+      FOR EACH ROW EXECUTE PROCEDURE article_tsvector_trigger();
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
