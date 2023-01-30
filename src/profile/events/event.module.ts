c7107a46b2b976ff75d3367d685b84905018e716
anhtt2211 <trananh22112001@gmail.com>
2023-01-31 00:33:41 +0700

    wip

--- src/profile/events/event.module.ts
+++ src/profile/events/event.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ReadConnection } from "../../config";
-import { UserEntity } from "../../user/user.entity";
 import { EventHandlers } from "../events";
+import { FollowsEntity } from "../follows.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity], ReadConnection), CqrsModule],
+  imports: [
+    TypeOrmModule.forFeature([FollowsEntity], ReadConnection),
+    CqrsModule,
+  ],
   providers: [...EventHandlers],
   controllers: [],
 })
