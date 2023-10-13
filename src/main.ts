2213ee8a46641716bab83e8bdc1e2a6f4e073676
anhtt2211 <trananh.2211.work@gmail.com>
2023-10-13 15:14:29 +0700

    bug: wrong import file path

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { json, urlencoded } from "express";
 import * as os from "os";
 
 import { ApplicationModule } from "./app.module";
-import { ArticleProjection } from "./article/application/projections";
-import { ProfileProjection } from "./profile/application/projections";
-import { UserProjection } from "./user/application/projections";
+import { ArticleProjection } from "./article/application/article.projection";
+import { ProfileProjection } from "./profile/application/profile.projection";
+import { UserProjection } from "./user/application/user.projection";
+import { ElasticSearchArticleProjection } from "./article/elastic-search-article.projection";
 
 async function executeProjection(app: INestApplication) {
   const articleProjection = app.get(ArticleProjection);
