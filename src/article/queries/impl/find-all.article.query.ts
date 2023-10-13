892da3754a4972f7142892f2eda84cdeb8252848
anhtt2211 <trananh.2211.work@gmail.com>
2023-10-13 14:45:21 +0700

    feat: apply CLEAN Architect for article-module

--- src/article/queries/impl/find-all.article.query.ts
+++
@@ -1,8 +0,0 @@
-import { ArticleFilters } from "../../dto/article-query";
-
-export class FindAllArticleQuery {
-  constructor(
-    public readonly userId: number,
-    public readonly query: ArticleFilters
-  ) {}
-}
