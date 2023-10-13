d8b8a5b44221ca6179932f02aafc748fcff27986
anhtt2211 <trananh.2211.work@gmail.com>
2023-10-13 14:58:52 +0700

    feat: separate db-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,60 +1,24 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import * as dotenv from "dotenv";
-import { AppController } from "./app.controller";
+
 import { ArticleModule } from "./article/article.module";
-import { READ_CONNECTION, WRITE_CONNECTION } from "./config";
+import { DatabaseModule } from "./database/database.module";
 import { MediaModule } from "./media/media.module";
 import { ProfileModule } from "./profile/profile.module";
 import { TagModule } from "./tag/tag.module";
 import { UserModule } from "./user/user.module";
 
-dotenv.config();
-
-const defaultOptions = {
-  type: process.env.DATABASE_ENGINE,
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  entities: [__dirname + process.env.TYPEORM_ENTITIES],
-  migrations: [__dirname + process.env.TYPEORM_MIGRATIONS],
-  logging: process.env.TYPEORM_LOGGING === "true",
-  synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
-  migrationsRun: process.env.TYPEORM_MIGRATION_RUN === "true",
-  migrationsTableName: "migrations",
-  cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
-  },
-};
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      name: WRITE_CONNECTION,
-      useFactory: () => ({
-        ...defaultOptions,
-        type: "postgres",
-        database: process.env.WRITE_DATABASE_NAME,
-      }),
-    }),
-    TypeOrmModule.forRootAsync({
-      name: READ_CONNECTION,
-      useFactory: () => ({
-        ...defaultOptions,
-        type: "postgres",
-        database: process.env.READ_DATABASE_NAME,
-      }),
-    }),
+    DatabaseModule,
     ArticleModule,
     UserModule,
     ProfileModule,
     TagModule,
     MediaModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class ApplicationModule {}
