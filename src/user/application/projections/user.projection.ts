17bc3bef4579e12fff6b4ad8afb208578237dce9
anhtt2211 <trananh.2211.work@gmail.com>
2023-10-15 09:42:22 +0700

    update: enhance folder structure

--- src/user/application/projections/user.projection.ts
+++ src/user/application/projections/user.projection.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { EventBus } from "@nestjs/cqrs";
-import { ConsumerService } from "../../rabbitmq/consumer.service";
-import { USER_QUEUE } from "../../rabbitmq/rabbitmq.constants";
-import { IMessage, IProjection } from "../core";
-import { MessageType } from "../core/enums/user.enum";
-import { UserCreatedEvent, UserUpdatedEvent } from "./events";
+
+import { ConsumerService } from "../../../rabbitmq/consumer.service";
+import { USER_QUEUE } from "../../../rabbitmq/rabbitmq.constants";
+import { IMessage, IProjection } from "../../core";
+import { MessageType } from "../../core/enums/user.enum";
+import { UserCreatedEvent, UserUpdatedEvent } from "../events";
 
 @Injectable()
 export class UserProjection implements IProjection {
