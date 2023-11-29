3a087f319f71b3d8e04f2961bc160aac033b711d
anhtt2211 <trananh.2211.work@gmail.com>
2023-11-29 10:45:38 +0700

    Refactor media file uploading and signed URL generation
    - Commented out code related to image upload to S3 in media.controller.ts
    - Removed the uploadS3 function in media.controller.ts
    - Kept the generate presigned Url function in media.controller.ts
    - Deleted the commented out code for uploading files in s3.service.ts
    - Removed the private method for getting a signed URL in s3.service.ts
    - Modified the `generatePresignedUrl` method in s3.service.ts

--- src/modules/media/application/services/s3.service.ts
+++ src/modules/media/application/services/s3.service.ts
@@ -32,40 +32,6 @@ export class S3Service {
     this.s3 = new AWS.S3();
   }
 
-  // async uploadFile(userId: number, file: Express.Multer.File): Promise<string> {
-  //   try {
-  //     const params: AWS.S3.Types.PutObjectRequest = {
-  //       Bucket: this.bucketName,
-  //       Body: file.buffer,
-  //       Key: `articles/${new Date().getTime()}-${file.originalname}`,
-  //       ACL: "public-read",
-  //     };
-
-  //     const fileUpload = await this.s3.upload(params).promise();
-
-  //     const fileEntity: FileEntity = new FileEntity({
-  //       name: file.originalname,
-  //       size: file.size,
-  //       mimeType: file.mimetype,
-  //       url: fileUpload.Location,
-  //       key: fileUpload.Key,
-  //       author: {
-  //         id: userId,
-  //       },
-  //     });
-  //     await this.fileWriteRepository.save(fileEntity);
-
-  //     this.articleRmqClient.emit<any, IPayloadFileCreated>(
-  //       { cmd: MessageCmd.FILE_CREATED },
-  //       { file: fileEntity }
-  //     );
-
-  //     return this.getSignedUrl(fileUpload.Key);
-  //   } catch (error) {
-  //     throw new HttpException("Cannot upload file", HttpStatus.BAD_REQUEST);
-  //   }
-  // }
-
   async generatePresignedUrl(userId: number): Promise<any> {
     const key = `user-uploads/${userId}/${new Date().getTime()}.jpg`;
     const url = `https://${this.bucketName}.s3.${this.awsRegion}.amazonaws.com/${key}`;
@@ -98,15 +64,6 @@ export class S3Service {
       url,
     };
   }
-
-  // Method to get a signed URL for private access
-  // private getSignedUrl(key: string, method: string = "getObject"): string {
-  //   return this.s3.getSignedUrl(method, {
-  //     Bucket: this.bucketName,
-  //     Key: key,
-  //     Expires: 60 * 5, // The URL will expire in 5 minutes
-  //   });
-  // }
 
   async downloadFile(key: string): Promise<AWS.S3.GetObjectOutput> {
     const params = {
--- src/modules/media/presentation/rest/media.controller.ts
+++ src/modules/media/presentation/rest/media.controller.ts
@@ -16,27 +16,6 @@ import { S3Service } from "../../application/services";
 export class MediaController {
   constructor(private readonly s3Service: S3Service) {}
 
-  // @ApiOperation({ summary: "Upload image to S3" })
-  // @ApiResponse({
-  //   status: 201,
-  //   description: "The image has been successfully uploaded.",
-  // })
-  // @ApiResponse({ status: 403, description: "Forbidden." })
-  // @UseInterceptors(FileInterceptor("file"))
-  // @Post("/img/upload")
-  // async uploadS3(
-  //   @User("id") userId: number,
-  //   @UploadedFile() file: Express.Multer.File
-  // ): Promise<MediaOutput> {
-  //   const uploadData = await this.s3Service.uploadFile(userId, file);
-
-  //   return {
-  //     media: {
-  //       url: uploadData,
-  //     },
-  //   };
-  // }
-
   @ApiOperation({ summary: "Generate presigned Url" })
   @ApiResponse({
     status: 201,
