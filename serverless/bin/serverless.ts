47e612d9e756b3412237434635bf7eaf55753719
anhtt2211 <trananh.2211.work@gmail.com>
2024-03-11 15:11:49 +0700

    chore: Remove serverless

--- serverless/bin/serverless.ts
+++
@@ -1,6 +0,0 @@
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { ServerlessStack } from "../lib/serverless-stack";
-
-const app = new cdk.App();
-new ServerlessStack(app, "ServerlessStack", {});
--- serverless/src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export enum EOpenSearchIndex {
-  ARTICLES = "articles",
-}
-
-export enum ESQSQueue {
-  CREATE_ARTICLE_QUEUE = "CREATE_ARTICLE_QUEUE",
-}
