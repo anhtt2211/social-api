9ca83dcca4f2989b2f55e8c559faf24e63800eeb
anhtt2211 <trananh.2211.work@gmail.com>
2024-03-11 15:14:41 +0700

    chore: wrong spell name

--- src/infrastructure/persistence/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-// export * from "./article.repository";
-// export * from "./block.repository";
-// export * from "./comment.repository";
-// export * from "./file.repository";
-// export * from "./follow.repository";
-// export * from "./user.repository";
