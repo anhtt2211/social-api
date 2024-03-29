f8a9396b591748a2c5badc0f07cd92408a18be2d
anhtt2211 <trananh.2211.work@gmail.com>
2024-03-29 13:39:17 +0700

    chore: enhance folder structure for cdc

--- cdc/deploy-configurations.sh
+++ cdc/deploy-configurations.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # setup elasticsearch indices
-curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/article?include_type_name=true -d @configs/es-article-index.json
+curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/article?include_type_name=true -d @configs/es-indexes/es-article-index.json
 
 # setup elasticsearch sink
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/es-article-sink.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/sink-connectors/es-article-sink.json
 
 # setup connector
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/postgres-article-source.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/source-connectors/postgres-article-source.json
